7d77ef0454ed420d66ea0d4f5f5bea796c110854
Jack Amadeo <jackamadeo@block.xyz>
2025-03-24 12:45:14 -0400

    Make the LLM client a plugin (#5)

--- src/ai_migrate/llm_providers/__init__.py
+++ src/ai_migrate/llm_providers/__init__.py
@@ -1,3 +1,8 @@
+from importlib.metadata import entry_points
+
 from .openai import OpenAIClient
 
-DefaultClient = OpenAIClient
+try:
+    DefaultClient = entry_points(group="ai_migrate")["default_llm_provider"].load()
+except KeyError:
+    DefaultClient = OpenAIClient
